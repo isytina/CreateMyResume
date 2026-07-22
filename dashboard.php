42b74e204a6045a5887fcca983a025ba6178e28e
isytina <asniza.amri@gmail.com>
2026-07-22 10:49:03 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -38,6 +38,8 @@ $recommendedJobs = 5;
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Dashboard - CreateMyResume</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
@@ -68,7 +70,7 @@ $recommendedJobs = 5;
 <div class="container py-5">
   <div class="row g-4">
 
-    <div class="col-lg-3">
+    <div class="col-lg-3 d-none d-lg-block">
       <?php include 'include/sidebar.php'; ?>
     </div>
 
