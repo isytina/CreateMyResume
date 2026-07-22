bb198557a6f6bf3018945485f27e774bcfd42a3c
isytina <asniza.amri@gmail.com>
2026-07-22 10:50:42 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -49,6 +49,8 @@ $profileImage = $_SESSION["profile_image"] ?? "";
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Profile - CreateMyResume</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
@@ -75,7 +77,7 @@ $profileImage = $_SESSION["profile_image"] ?? "";
 <div class="container py-5">
   <div class="row g-4">
 
-    <div class="col-lg-3">
+    <div class="col-lg-3 d-none d-lg-block">
       <?php include 'include/sidebar.php'; ?>
     </div>
 
